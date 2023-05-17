f658544934addbbba899b56919f23d0a93cb4325
T-Dynamos <anshdadwal298@gmail.com>
2023-05-17 17:24:59 +0530

    palette: update neutral hue to 6

--- materialyoucolor/palettes/core_palette.py
+++ materialyoucolor/palettes/core_palette.py
@@ -9,7 +9,7 @@ class CorePalette:
         self.a1 = TonalPalette.fromHueAndChroma(hue, max(48, hct.chroma))
         self.a2 = TonalPalette.fromHueAndChroma(hue, 16)
         self.a3 = TonalPalette.fromHueAndChroma(hue + 60, 24)
-        self.n1 = TonalPalette.fromHueAndChroma(hue, 4)
+        self.n1 = TonalPalette.fromHueAndChroma(hue, 6)
         self.n2 = TonalPalette.fromHueAndChroma(hue, 8)
         self.error = TonalPalette.fromHueAndChroma(25, 84)
 
